76c33ffdf587335b6d80b30960c067baf2071910
Beomki Yeo <yeobk1202@kaist.ac.kr>
2018-11-23 21:37:03 +0900

    add some lines

--- DrawHistoGraph/DrawHisto1.C
+++ DrawHistoGraph/DrawHisto1.C
@@ -1,3 +1,15 @@
 void DrawHisto1(){
 
+  TFile *f = TFile::Open("energy_momentum.root");
+  TTree *t_em = (TTree*)f->Get("t_em");
+  TTree *t_mu = (TTree*)f->Get("t_mu");  
+
+  // apply some calculation
+  t_em->Draw("ene_em/mom_mu");
+
+  // Conditinoal Plot
+  t_em->Draw("ene_em");
+
+  // Plot option 
+ 
 }
