420f86210c7788dd87775fa299be9702cde57ed2
leptonist <physics.lepton@gmail.com>
2018-11-27 03:27:13 +0900

    Finish TMVA project files

--- TMVA/TMVAClassification.C
+++ TMVA/TMVAClassification.C
@@ -1,83 +1,51 @@
-#include <cstdlib>
-#include <iostream>
-#include <map>
-#include <string>
-
 void TMVAClassification( TString myMethodList = "" )
 {
    // This loads the library
    TMVA::Tools::Instance();
 
-   // Default MVA methods to be trained + tested
-   std::map<std::string,int> Use;
-
-   // Neural Networks (all are feed-forward Multilayer Perceptrons)
-   Use["MLP"]             = 1; // Recommended ANN
-   // Support Vector Machine
-   Use["SVM"]             = 1;
-   // Boosted Decision Trees
-   Use["BDT"]             = 1; // uses Adaptive Boost
-
+   // This loads the input file for machine learning
    TFile * input = TFile::Open("tmva_input.root", "read");
    
    // Register the training and test trees
-
+   // signal     -> electron
+   // background -> muon
    TTree *signal         = (TTree*)input->Get("t_em");
    TTree *background     = (TTree*)input->Get("t_mu");
 
-   // Create a ROOT output file where TMVA will store ntuples, histograms, etc.
-
+   // Create a output file where TMVA results will be stored 
    TString outputFileName = "tmva_output.root";
    TFile* output = TFile::Open( outputFileName, "recreate" );
 
-   // Create the factory object. Later you can choose the methods
-   // whose performance you'd like to investigate. The factory is
-   // the only TMVA object you have to interact with
-   //
-   // The first argument is the base of the name of all the
-   // weightfiles in the directory weight/
-   //
-   // The second argument is the output file for the training results
-   // All TMVA output can be suppressed by removing the "!" (not) in
-   // front of the "Silent" argument in the option string
-   TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", output,
-                                               "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
+   // Create the factory object where methods for machine learning are contained
+   TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification",
+					       output,
+					       "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
 
+   // Create the dataloader where train/test objects is handled
    TMVA::DataLoader *dataloader=new TMVA::DataLoader("dataset");
-   dataloader->AddVariable( "energy := energy", 'D' );
-   dataloader->AddVariable( "momentum := momentum", 'D' );
 
-   // global event weights per tree (see below for setting event-wise weights)
-   Double_t signalWeight     = 1.0;
-   Double_t backgroundWeight = 1.0;
+   // Add varaible for machine learning in Float ('F') type
+   dataloader->AddVariable( "energy := energy", 'F' );
+   dataloader->AddVariable( "momentum := momentum", 'F' );
+
+   // Set event weights: Just make it 1 
+   Float_t signalWeight     = 1.0;
+   Float_t backgroundWeight = 1.0;
 
    // You can add an arbitrary number of signal or background trees
    dataloader->AddSignalTree    ( signal    ,     signalWeight );
    dataloader->AddBackgroundTree( background, backgroundWeight );
 
-   TCut mycuts = ""; // for example: TCut mycuts = "abs(var1)<0.5 && abs(var2-0.5)<1";
-   TCut mycutb = ""; // for example: TCut mycutb = "abs(var1)<0.5";
-
-   dataloader->PrepareTrainingAndTestTree( mycuts, mycutb,
-                                        "nTrain_Signal=1000:nTrain_Background=1000:SplitMode=Random:NormMode=NumEvents:!V" );
+   dataloader->PrepareTrainingAndTestTree( "", "",
+					   "nTrain_Signal=5000:nTrain_Background=5000:nTest_Signal=5000:nTest_Background=5000:SplitMode=Random:NormMode=NumEvents:!V" );
 
    // ### Book MVA methods
-   
-   // TMVA ANN: MLP (recommended ANN) -- all ANNs in TMVA are Multilayer Perceptrons
-   if (Use["MLP"])
-      factory->BookMethod( dataloader, TMVA::Types::kMLP, "MLP", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator" );
+   // Every parameter for machine learning is roughly optimized already so you don't have to edit
+   factory->BookMethod( dataloader, TMVA::Types::kMLP, "MLP", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator" );
+   factory->BookMethod( dataloader, TMVA::Types::kSVM, "SVM", "Gamma=0.25:Tol=0.001:VarTransform=Norm" );
+   factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDT",
+			"!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" );
 
-   // Support Vector Machine
-   if (Use["SVM"])
-      factory->BookMethod( dataloader, TMVA::Types::kSVM, "SVM", "Gamma=0.25:Tol=0.001:VarTransform=Norm" );
-
-
-   if (Use["BDT"])  // Adaptive Boost
-      factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDT",
-                           "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" );
-
-   // Now you can tell the factory to train, test, and evaluate the MVAs
-   //
    // Train MVAs using the set of training events
    factory->TrainAllMethods();
 
@@ -87,34 +55,12 @@ void TMVAClassification( TString myMethodList = "" )
    // Evaluate and compare performance of all configured MVAs
    factory->EvaluateAllMethods();
 
-   // --------------------------------------------------------------
-
    // Save the output
    output->Close();
-
-   std::cout << "==> Wrote root file: " << output->GetName() << std::endl;
-   std::cout << "==> TMVAClassification is done!" << std::endl;
 
    delete factory;
    delete dataloader;
 
-   // Launch the GUI for the root macros
-   //if (!gROOT->IsBatch())
+   // Pop-up GUI of TMVA result
    TMVA::TMVAGui( outputFileName );
-
-   //return 0;
 }
-/*
-int main( int argc, char** argv )
-{
-   // Select methods (don't look at this code - not of interest)
-   TString methodList;
-   for (int i=1; i<argc; i++) {
-      TString regMethod(argv[i]);
-      if(regMethod=="-b" || regMethod=="--batch") continue;
-      if (!methodList.IsNull()) methodList += TString(",");
-      methodList += regMethod;
-   }
-   return TMVAClassification(methodList);
-}
-*/
